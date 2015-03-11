ebef8a4c1753c704723545b8b7e346cf8ab5cb47
sancho-munoz <samunozj@alumnos.unex.es>
2015-03-11 13:08:21 +0100

    Minor fixes

--- Assets/Prefabs/Teleport/Teleport.cs
+++ Assets/Prefabs/Teleport/Teleport.cs
@@ -5,8 +5,10 @@ public class Teleport : MonoBehaviour {
 	public Teleport destination;
 
 	public void OnTriggerEnter(Collider other){
-		Vector3 position = destination.gameObject.transform.position;
-		position.x = position.x + 3;
-		other.gameObject.transform.position = position;
+		if(other.gameObject.tag == "Player"){
+			Vector3 position = destination.gameObject.transform.position;
+			position.x = position.x + 3;
+			other.gameObject.transform.position = position;
+		}
 	}
 }
