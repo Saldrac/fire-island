79bde42acf3a9c560fd7a29eef88ac6f3aaed1c8
Eldir <lothvorn@gmail.com>
2015-03-16 13:27:02 +0100

    Cambio en DayNightCycle
    modificado para que el giro se haga en base 0-360. El cambio de ciclo se
    comprueba con la variable angulos; de 0 a 180 es día, en cualquier otro
    caso es noche.

--- Assets/Prefabs/Day and night/DayNightCycle.cs
+++ Assets/Prefabs/Day and night/DayNightCycle.cs
@@ -9,25 +9,42 @@ public class DayNightCycle : MonoBehaviour {
 
 	public void Start(){
 		gamedata = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameData>();
-		angles.x = 0;
+
+		angles = transform.eulerAngles;
+
+/*	//comentado por Jose para el tema de rotaciones
+ 		angles.x = 0;
 		angles.y = 0;
 		angles.z = 0;
+*/
 	}
 
 
 	// Update is called once per fixed frame
 	void FixedUpdate () {
-		angles.x = (angles.x + rotation_speed)%360;
-		transform.localEulerAngles = angles;
+/*		//comentado por Jose para el tema de rotaciones
+		  angles.x = (angles.x + rotation_speed)%360;
+			transform.localEulerAngles = angles;
+*/
+		angles.x = (angles.x + rotation_speed) %360;
+		transform.eulerAngles = angles;
+		Debug.Log ("my angles " + angles.x + "T. Angles " + transform.eulerAngles.x );
+
 		CheckChangeCicle();
 	}
 
 	void CheckChangeCicle(){
+/* // comentado por Jose para el tema de las rotaciones
 		if(transform.localEulerAngles.y == 0)
 			gamedata.setDay(true);
 		else
 			gamedata.setDay(false);
-
-
+*/
+		if(angles.x >= 0 && angles.x<= 180)
+			gamedata.setDay(true);
+		else
+			gamedata.setDay(false);
+		
+		
 		}
 }
