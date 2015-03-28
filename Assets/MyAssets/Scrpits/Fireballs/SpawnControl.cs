c9a3b0c36bb13da4ad4b0e57e0dc2209735e264c
Eldir <lothvorn@gmail.com>
2015-03-28 12:56:13 +0100

    Modificacion en la trampa de bolas
    Ahora los triggers tienen cooldown y el eraser funciona. Ojo que el
    problema era que estaban en layers distintos y por eso no se detectaba
    la colision. Para arreglarlo he puesto el el eraser en el layer Traps y
    he modificado la matriz de colisiones para que las traps colisionen
    entre ellas. Esto PUEDE que este estropeando otra cosa.

--- Assets/MyAssets/Scrpits/Fireballs/SpawnControl.cs
+++ Assets/MyAssets/Scrpits/Fireballs/SpawnControl.cs
@@ -2,21 +2,39 @@
 using System.Collections;
 
 public class SpawnControl : MonoBehaviour {
-
+	//vars to expose
+	public float coolingTime = 15;
+	
+	//to hide
+	public float coolingTimer = 0;
+	public bool cooling = false;
+	
 	public FireballGroupController groupController;
 	// Use this for initialization
 	void Start () {
-	
+		coolingTimer = coolingTime;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (cooling){
+			coolingTimer -= Time.deltaTime;
+			if (coolingTimer <= 0){
+				coolingTimer = coolingTime;
+				cooling = false;
+			}
+		}
+		
+		
 	}
-
+	
 	public void OnTriggerEnter(Collider collision){
-		if (collision.gameObject.tag == "Player")
+		if ((collision.gameObject.tag == "Player") && (!cooling)){
 			groupController.SpawnBall ();
-
+			cooling = true;
+			
+		}
+		
 	}
 }
