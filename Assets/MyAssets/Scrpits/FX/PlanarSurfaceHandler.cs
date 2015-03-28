2f9c28baacac4e271d8d4391814748ad3a6dc9e5
Eldir <lothvorn@gmail.com>
2015-03-28 13:35:47 +0100

    Añadido prefab de teleport orientandose al jugador
    Es 'TeleportGroup'. Ha sido necesario añadir algunos elementos al
    prefab. TeleportGroup debe sustituir al viejo Teleport

---
+++ Assets/MyAssets/Scrpits/FX/PlanarSurfaceHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanarSurfaceHandler : MonoBehaviour {
+	public GameObject target;
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currentRotation = transform.eulerAngles;
+		transform.LookAt (target.transform);
+		transform.eulerAngles =  new Vector3 (0f, transform.eulerAngles.y, 0f);
+
+
+	}
+}
